5d1a1455bb399ab68c96fae92afaf39906f64854
waddle@waddlelab.com <waddle@waddlelab.com>
2024-10-07 16:51:37 +0900

    feat:sharing page style

--- src/page/Sharing/Style.js
+++ src/page/Sharing/Style.js
@@ -11,69 +11,94 @@ export const Layout = css`
 `;
 
 export const H1 = css`
-    font-size: 20px;  /* Changed to match the size of the "모임 페이지 가기" button text */
+    font-size: 22px;
+    color: var(--G10, #000);
+    font-size: 22px;
+    font-style: normal;
     font-weight: 700;
-    color: #0F1720;
-    margin-bottom: 20px;
+    line-height: 26px; /* 118.182% */
+    letter-spacing: -0.22px;
     text-align: left;
     width: 750px;
+`;
 
-    @media (max-width: 800px) {
-        font-size: 18px;  /* Adjust font size for smaller screens */
-        width: auto;
-    }
+export const LogoContainer = css`
+    display: flex;
+    padding: 80px 0px;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 2px;
+    align-self: stretch;
+`;
+
+export const LogoImage = css`
+    width: 750px;
+    height: 280px;
+    display: block;
 `;
 
 export const UrlBox = css`
     width: 750px;
     height: 52px;
     display: flex;
-    align-items: center;
+    margin-top: 20px;
     margin-bottom: 30px;
 
     input {
-        border: 1px solid #F1F2F4;
-        width: calc(100% - 188px); /* Adjusted width to accommodate button and spacing */
-        height: 100%;
-        border-radius: 10px;
+        width: 562px;
+        height: 52px;
+        padding: 15px 18px;
+        gap: 10px;
+        align-items: center;
+        border-radius: 8px;
+        border: 1.5px solid var(--G3, #F1F2F4);
+        background: var(--G1, #FFF);
         padding-left: 10px;
-        outline: none;
-        color: #0F1720;
-        font-size: 16px;
+        color: var(--G10, #000);
+        font-size: 18px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 22px; /* 122.222% */
+        letter-spacing: -0.18px;
     }
 
     button {
+        display: flex;
         width: 180px;
         height: 52px;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+        flex-shrink: 0;
         margin-left: 8px;
-        border: 1px solid #0F1720;
-        background-color: transparent;
         border-radius: 8px;
-        cursor: pointer;
-        font-size: 16px;
-        font-weight: 600;
-        text-align: center;
-
-        @media (max-width: 800px) {
-            width: 100%;
-            margin-top: 10px;
-            font-size: 18px;
-        }
+        border: 1px solid var(--G10, #000);
+        background: var(--G1, #FFF);
+        color: var(--G10, #000);
+        font-size: 18px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: 30px; /* 166.667% */
+        letter-spacing: -0.18px;
     }
 `;
 
 export const Btn = css`
     width: 750px;
     height: 68px;
-    border: none;
-    background-color: #000000;
-    color: #FFFFFF;
-    font-size: 20px;
-    font-weight: 600;
-    border-radius: 6px;
-
-    @media (max-width: 800px) {
-        width: 92%;
-        font-size: 18px;
-    }
+    padding: 20px 0px;
+    justify-content: center;
+    align-items: center;
+    flex-shrink: 0;
+    border-radius: 8px;
+    border: 1px solid var(--G10, #000);
+    background: var(--G10, #000);
+    color: var(--G1, #FFF);
+    font-family: "Noto Sans";
+    font-size: 22px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 26px; /* 118.182% */
+    letter-spacing: -0.22px;
 `;
